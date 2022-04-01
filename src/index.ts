d825645276ba926d001204e6c353cd1f7e1c0ada
hieu.dv <hieu.dv@teko.vn>
2022-04-02 01:50:20 +0700

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors({
-    origin: `${process.env.BASE_URL}`,
+    origin: '*',
     methods: "*",
     credentials: true
 }))
