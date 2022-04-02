d565b3bfa781c5fda701ccf7749731c73ead8120
hieu.dv <hieu.dv@teko.vn>
2022-04-02 16:07:28 +0700

    alow host apis google cors

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors({
-    origin: [`${process.env.BASE_URL}`],
+    origin: [`${process.env.BASE_URL}`, 'https://apis.google.com'],
     credentials: true
 }))
 app.use(morgan('dev'))
